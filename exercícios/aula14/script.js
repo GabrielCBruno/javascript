cb02adaa3efe9a40a24d442ee87aba20135e82c1
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-10-30 19:38:10 -0300

    Criei o exercício 1 (aula 14)

--- exercícios/aula14/script.js
+++ exercícios/aula14/script.js
@@ -1,1 +1,22 @@
-alert("Olá")
+var msg = document.querySelector("#msg")
+var img = document.querySelector("#imagem")
+var data = new Date()
+var hora = data.getHours()
+
+function carregar() {
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if ((hora >= 0) && (hora < 12)) {
+        //BOM DIA
+        img.src = 'imagens/manha.jpg'
+        document.body.style.background = '#dbcd72'
+
+    }else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.background = 'rgb(228, 106, 6)'
+    }else {
+        //BOA NOITE
+        img.src = 'imagens/noite.jpg'
+        document.body.style.background = 'rgb(70, 142, 236)'
+    }
+}
