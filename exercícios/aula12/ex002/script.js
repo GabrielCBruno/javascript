edc3eb012ae57ad787e721eb3d221e03d2428256
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-10-31 16:36:26 -0300

    Atualização ex002 (aula 12)

--- exercícios/aula12/ex002/script.js
+++ exercícios/aula12/ex002/script.js
@@ -1,14 +1,13 @@
-alert(teste)
-
-function Verificar() {
+function Validar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.querySelector('#txtano')
+    var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
 
-    if(fano.value.length == 0 || Number(fano.value) > ano) {
-        document.alert('[ERRO] Verificar os dados e tente novamente.')
+    if ((fano.value == 0) || (fano.value > ano)) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
     }else {
-
+        
     }
+
 }
