f453f94e2fa37962f022a3cf86a9bc7904b46fc6
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-10-31 23:08:13 -0300

    desafio 01

---
+++ desafios/ds001/scripts/scripts.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById('bt1')
+let sessao = document.getElementById('sec')
+
+function alterar() {
+    botao.style.display = "none"
+    sessao.style.backgroundImage = "../imagens/julioz.jpg"
+}
