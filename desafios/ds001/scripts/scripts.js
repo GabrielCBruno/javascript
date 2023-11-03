b3e4ccd5f2953143239ac37ab25afa35d64839a9
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-11-02 23:23:11 -0300

    Atualização (desafio 1)

--- desafios/ds001/scripts/scripts.js
+++ desafios/ds001/scripts/scripts.js
@@ -1,7 +1,10 @@
 let botao = document.getElementById('bt1')
 let sessao = document.getElementById('sec')
+let corpo = document.getElementById('corpo')
 
 function alterar() {
     botao.style.display = "none"
-    sessao.style.backgroundImage = "../imagens/julioz.jpg"
+    //sessao.style.backgroundImage = "../imagens/julioz.jpg"
+
+    sessao.style.backgroundImage = "green"
 }
