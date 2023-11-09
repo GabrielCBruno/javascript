a674e05fdffe7c4853bdabd295c689aa13df781e
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2023-11-09 19:14:22 -0300

    Criação do 2 site

--- desafios/ds001/scripts/scripts.js
+++ desafios/ds001/scripts/scripts.js
@@ -5,6 +5,5 @@ let corpo = document.getElementById('corpo')
 function alterar() {
     botao.style.display = "none"
     //sessao.style.backgroundImage = "../imagens/julioz.jpg"
-
     sessao.style.backgroundImage = "green"
 }
