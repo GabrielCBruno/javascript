20f81fae2d11dcce6b7991b99d2491636f0bd548
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-01-09 14:08:53 -0300

    Remoção desafio 01

--- desafios/ds001/scripts/scripts.js
+++
@@ -1,9 +0,0 @@
-let botao = document.getElementById('bt1')
-let sessao = document.getElementById('sec')
-let corpo = document.getElementById('corpo')
-
-function alterar() {
-    botao.style.display = "none"
-    //sessao.style.backgroundImage = "../imagens/julioz.jpg"
-    sessao.style.backgroundImage = "green"
-}
