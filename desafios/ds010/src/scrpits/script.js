33b161156378d49eebdae11254033f96acff0029
GabrielCBruno <142371512+GabrielCBruno@users.noreply.github.com>
2024-01-21 16:05:10 -0300

    Alteração javascript

--- desafios/ds010/src/scrpits/script.js
+++ desafios/ds010/src/scrpits/script.js
@@ -5,5 +5,5 @@ function calcular() {
     let b = Number(prompt(`Qual é o valor de b?`))
     let c = Number(prompt(`Qual é o valor de c?`))
     let delta = (b ** 2) - 4 * a * c
-    saida.innerHTML += `<p>Resolvendo Bhaskara <br>A equação atual é 3x² + 6x + 2= 0 <br>O cálculo realizado será Δ = 6² - 4 . 3 . 2 <br>O valor calculado foi Δ = ${delta}</p>`
+    saida.innerHTML += `<p>Resolvendo Bhaskara <br>A equação atual é ${a}x² + ${b}x + ${c}= 0 <br>O cálculo realizado será Δ = ${b}² - 4 . ${a} . ${c} <br>O valor calculado foi Δ = ${delta}</p>`
 }
