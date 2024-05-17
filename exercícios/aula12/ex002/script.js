c7626a01a363bc4d0b289cf85aaff46c91bf407a
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-05-17 19:19:19 -0300

    Finalização do exerício 2 aula 12

--- exercícios/aula12/ex002/script.js
+++ exercícios/aula12/ex002/script.js
@@ -1,13 +1,16 @@
+"use stect"
+
 function Validar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('txtano')
+    let res = document.getElementById('res')
 
-    if ((fano.value == 0) || (fano.value > ano)) {
+    if ((fano.value == 0) || (fano.value < 1910) || (fano.value > ano)) {
         window.alert("[ERRO] Verifique os dados e tente novamente!")
+        fano.value = 0
     }else {
-        
+        let idade = ano - (Number) (fano.value)
+        res.innerHTML = "Você tem atualmente ou irá fazer " + idade + " anos."
     }
-
 }
