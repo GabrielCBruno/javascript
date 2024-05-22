d851b679031ac47d40a86038c10d3d714b5c633a
Gabriel <142371512+GabrielCBruno@users.noreply.github.com>
2024-05-22 19:50:26 -0300

    Segunda Correção

--- exercícios/aula29/scripts.js
+++ exercícios/aula29/scripts.js
@@ -1,8 +1,8 @@
 "use strect"
 
-/*Evento: é toda ação que ocorre no site. Exemplo: entrada de mouse, clique, saida de mouse, etc.*/
+/*Evento: é toda ação/interação que ocorre no site. Exemplo: entrada de mouse, clique, saida de mouse, etc.*/
 
-/*Para capturar um evento usa-se o addEventListener, no primeiro parâmetro é passado o evento que será capturado, no segundo uma função anônima.*/
+/*Para capturar um evento usa-se o addEventListener(), no primeiro parâmetro é passado o evento que será capturado, no segundo uma função anônima.*/
 
 const c1 = [...document.querySelectorAll(".botao")]
 
@@ -10,7 +10,7 @@ c1.map((el)=>{
     el.addEventListener("click",(evt)=>{
         const el= evt.target
         el.classList.add("botao")
-        alert("teste")
+        alert("Você clicou no botao " + el.innerHTML)
     })
 })
 
